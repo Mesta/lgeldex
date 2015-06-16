aa864491c1bea4d6110b37c702f228850d8e32be
Jérémy Lardet <contact@jeremylardet.fr>
2015-06-17 01:29:57 +0200

    Captcha ok

--- app/controllers/versus_config_controller.rb
+++ app/controllers/versus_config_controller.rb
@@ -6,13 +6,17 @@ class VersusConfigController < ApplicationController
   end
 
   def create
-    @versus_config = VersusConfigForm.new
+    if verify_recaptcha
+      @versus_config = VersusConfigForm.new
 
-    if @versus_config.update(versus_config_params)
-      session[:current_user_id] = { "is_serious" => @versus_config.is_serious,  "mode" => @versus_config.mode }
-      redirect_to versus_path
+      if @versus_config.update(versus_config_params)
+        session[:current_user_id] = { "is_serious" => @versus_config.is_serious,  "mode" => @versus_config.mode }
+        redirect_to versus_path
+      else
+        flash[:danger] = "La configuration a échoué. On recommence !"
+        redirect_to versus_config_path
+      end
     else
-      flash[:danger] = "La configuration a échoué. On recommence !"
       redirect_to versus_config_path
     end
   end
