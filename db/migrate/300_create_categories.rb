d3dda304305c33d177d668dd8a7596e464286f54
Jérémy Lardet <contact@jeremylardet.fr>
2015-06-21 20:10:24 +0200

    Upload OK TODO : Voir dimension attendus avec Vorap

--- db/migrate/300_create_categories.rb
+++ db/migrate/300_create_categories.rb
@@ -1,12 +1,11 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :nom, null: false
-      t.string :question, null: false
-      t.string :description, null: true
-      t.boolean :is_serious, default: false
-
-      t.timestamps null: false
+      t.string :nom,          null: false
+      t.string :question,     null: false
+      t.string :description,  null: true
+      t.boolean :is_serious,  default: false
+      t.timestamps            null: false
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,15 +38,24 @@ jm.push(JoueurMode.create joueur: j[3], mode: m[2])
 jm.push(JoueurMode.create joueur: j[4], mode: m[2])
 
 c = Array.new
-c.push(Category.create nom: 'Loup'          , question: 'Qui a les dents les plus longues ?', is_serious: true)
-c.push(Category.create nom: 'Innocent'      , question: 'Qui est le plus innocent ?', is_serious: true)
-c.push(Category.create nom: 'Pseudo'        , question: 'Qui a le meilleur pseudo ?', is_serious: false)
-c.push(Category.create nom: 'Popularité'    , question: 'Quel est le joueur le plus populaire ?', is_serious: false)
-c.push(Category.create nom: 'Spécialité'    , question: 'Qui joue la meilleur spécialité ?', is_serious: true)
-c.push(Category.create nom: 'Argumentateur' , question: 'Qui est le plus convaincant ?', is_serious: true)
-c.push(Category.create nom: 'Invisible'     , question: 'Lequel de ces joueurs est le plus invisible ?', is_serious: false)
-c.push(Category.create nom: 'Sale'          , question: "Lequel de ces joueurs est le plus sale ?", is_serious: false)
-c.push(Category.create nom: 'Présent'       , question: 'Quel est le joueur le plus présent ?', is_serious: false)
+c.push(Category.create nom: 'Loup'          , question: 'Qui a les dents les plus longues ?', is_serious: true,
+                       description: "Vous devez ici choisir le joueur qui, selon vous, est le meilleur loup.")
+c.push(Category.create nom: 'Innocent'      , question: 'Qui est le plus innocent ?', is_serious: true,
+                       description: "Vous devez ici choisir le joueur qui, selon vous, est le meilleur innocent.")
+c.push(Category.create nom: 'Pseudo'        , question: 'Qui a le meilleur pseudo ?', is_serious: false,
+                       description: "Vous devez ici choisir le joueur qui, selon vous, a le meilleur pseudo.")
+c.push(Category.create nom: 'Popularité'    , question: 'Quel est le joueur le plus populaire ?', is_serious: false,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, est le plus populaire.")
+c.push(Category.create nom: 'Spécialité'    , question: 'Qui joue la meilleur spécialité ?', is_serious: true,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, joue les mieux les spécialités du jeu.")
+c.push(Category.create nom: 'Argumentateur' , question: 'Qui est le plus convaincant ?', is_serious: true,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, a les meilleurs arguments en jeu.")
+c.push(Category.create nom: 'Invisible'     , question: 'Lequel de ces joueurs est le plus invisible ?', is_serious: false,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, est le plus discret.")
+c.push(Category.create nom: 'Sale'          , question: "Lequel de ces joueurs est le plus sale ?", is_serious: false,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, est le plus sale en jeu.")
+c.push(Category.create nom: 'Présent'       , question: 'Quel est le joueur le plus présent ?', is_serious: false,
+                       description: "Vous devez choisir ici le joueur qui, selon vous, est le plus présent en ligne.")
 
 c.each do |categ|
   jm.each do |joueur_mode|
