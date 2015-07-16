57d4a4ab6fd9e2a32626f3fe3164907494999819
Jérémy Lardet <contact@jeremylardet.fr>
2015-07-17 00:39:44 +0200

    Hotfix

--- app/controllers/versus_controller.rb
+++ app/controllers/versus_controller.rb
@@ -26,6 +26,7 @@ class VersusController < ApplicationController
   end
 
   def new
+    byebug
     # Check configuration of versus
     is_serious  = session[:current_user_id]["is_serious"]
     mode        = session[:current_user_id]["mode"]
@@ -43,30 +44,29 @@ class VersusController < ApplicationController
             :value => 20,
             :expires => 1.days.from_now
         }
-      else # if it exist
+      end
 
-        # If allow vote is 0 : redirect to config form with the correct flash message
-        if cookies[:user].to_i == 0
-          flash[:info] = "Vous n'avez plus de jetons pour voter aujourd'hui. Repassez demain !"
+      # If allow vote is 0 : redirect to config form with the correct flash message
+      if cookies[:user].to_i == 0
+        flash[:info] = "Vous n'avez plus de jetons pour voter aujourd'hui. Repassez demain !"
+        redirect_to versus_config_path
+
+      else # else player is allow to vote
+
+        # Check if there is enough player to vote for
+        if JoueurMode.where(mode: mode).count >= 2
+          # Instanciate a new form vote
+          @versus = VersusForm.new(is_serious, mode)
+          @versus.config()
+
+          session[:current_user_id] = { "category"  => @versus.categorie.id,
+                                        "joueur_1"  => @versus.joueur_1.id,
+                                        "joueur_2"  => @versus.joueur_2.id,
+                                        "is_serious" => is_serious, "mode" => mode,
+                                        "passer" => session[:current_user_id]["passer"]}
+        else
+          flash[:info] = "Il n'y a pas encore assez de joueurs. Un peu de patience, ça va venir :-)"
           redirect_to versus_config_path
-
-        else # else player is allow to vote
-
-          # Check if there is enough player to vote for
-          if JoueurMode.where(mode: mode).count >= 2
-            # Instanciate a new form vote
-            @versus = VersusForm.new(is_serious, mode)
-            @versus.config()
-
-            session[:current_user_id] = { "category"  => @versus.categorie.id,
-                                          "joueur_1"  => @versus.joueur_1.id,
-                                          "joueur_2"  => @versus.joueur_2.id,
-                                          "is_serious" => is_serious, "mode" => mode,
-                                          "passer" => session[:current_user_id]["passer"]}
-          else
-            flash[:info] = "Il n'y a pas encore assez de joueurs. Un peu de patience, ça va venir :-)"
-            redirect_to versus_config_path
-          end
         end
       end
     end
